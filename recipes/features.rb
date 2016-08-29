c731415606996156a49d3f926dbb3b99c40b63d1
Jason Scott <jsco2t@outlook.com>
2016-08-28 21:22:22 -0700

    removing stale code

--- recipes/features.rb
+++ recipes/features.rb
@@ -6,41 +6,6 @@
 #
 # License: MIT
 #
-=begin
-%w{
-  IIS-WebServerRole
-  IIS-WebServer
-  IIS-WebServerManagementTools
-  IIS-ManagementConsole
-  IIS-CommonHttpFeatures
-  IIS-StaticContent
-  IIS-DefaultDocument
-  IIS-DirectoryBrowsing
-  IIS-HttpErrors
-  IIS-HttpRedirect
-  IIS-ApplicationDevelopment
-  IIS-ISAPIExtensions
-  IIS-Security
-  IIS-RequestFiltering
-  IIS-NetFxExtensibility
-  IIS-BasicAuthentication
-  IIS-Performance
-  IIS-HttpCompressionDynamic
-  IIS-HttpCompressionStatic
-  IIS-ISAPIFilter
-  NetFx3ServerFeatures
-  NetFx3
-  NetFx4Extended-ASPNET45
-  IIS-NetFxExtensibility45
-  IIS-ASPNET45
-}.each do |feature|
-  windows_feature feature do
-    action :install
-    all true
-  end
-end
-=end
-
 =begin #small test set
 features_toinstall = [
   'IIS-WebServerRole',
